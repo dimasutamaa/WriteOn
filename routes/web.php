148ad427b02d87b167b1016108c079e5fdf5de60
dimasutamaa <dimasutamapa@gmail.com>
2024-10-05 12:33:28 +0700

    Updating Breeze Layout

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')
+    ->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
