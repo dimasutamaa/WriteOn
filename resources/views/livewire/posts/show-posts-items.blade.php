f8f4f245e3457b258b4ae8228559c001174fd33e
dimasutamaa <dimasutamapa@gmail.com>
2024-10-06 15:54:03 +0700

    search blog

--- resources/views/livewire/posts/show-posts-items.blade.php
+++ resources/views/livewire/posts/show-posts-items.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use Livewire\Attributes\Url;
+use Livewire\Attributes\On;
 use Livewire\Volt\Component;
 use Livewire\WithPagination;
 use App\Models\Post;
@@ -11,9 +12,18 @@ new class extends Component {
     #[Url]
     public $sort = 'desc';
 
+    #[Url]
+    public $search = '';
+
     public function setSort($sort)
     {
         $this->sort = $sort === 'desc' ? 'desc' : 'asc';
+    }
+
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
     }
 
     public function with(): array
@@ -21,6 +31,7 @@ new class extends Component {
         return [
             'posts' => Post::published()
                 ->orderBy('published_at', $this->sort)
+                ->where('title', 'like', "%{$this->search}%")
                 ->paginate(5),
         ];
     }
@@ -28,7 +39,7 @@ new class extends Component {
 
 <div class="px-3 py-6 lg:px-7">
     <div class="flex items-center justify-between border-b border-gray-100">
-        <div id="filter-selector" class="flex items-center space-x-4 font-light ">
+        <div class="flex items-center space-x-4 font-light ">
             <x-filter-btn :active="$sort === 'desc'" wire:click="setSort('desc')">
                 {{ __('Latest') }}
             </x-filter-btn>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,22 +6,7 @@
                     <livewire:posts.show-posts-items />
                 </div>
                 <div class="sticky top-0 h-screen col-span-4 px-3 py-6 pt-10 space-y-10 border-t border-gray-100 border-t-gray-100 md:border-t-none md:col-span-1 md:px-6 md:border-l">
-                    <div>
-                        <h3 class="mb-3 text-lg font-semibold text-gray-900 ">Search</h3>
-                        <div class="flex items-center px-3 py-2 mb-3 bg-white w-52 rounded-2xl">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-500">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                </svg>
-                            </span>
-                            <input
-                                class="w-40 ml-1 text-xs text-gray-800 bg-transparent border-none outline-none focus:outline-none focus:border-none focus:ring-0 placeholder:text-gray-400"
-                                type="text" placeholder="Search">
-                        </div>
-                    </div>
-
+                    <livewire:posts.search-box />
                     <div>
                         <h3 class="mb-3 text-lg font-semibold text-gray-900">Recommended Topics</h3>
                         <div class="flex flex-wrap justify-start topics">
