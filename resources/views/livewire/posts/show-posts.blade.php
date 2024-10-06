813a1e05de24a7c6ffb4e87c31e3b0baa63a4d61
dimasutamaa <dimasutamapa@gmail.com>
2024-10-06 13:45:06 +0700

    add wire key to post-card components

--- resources/views/livewire/posts/show-posts.blade.php
+++ resources/views/livewire/posts/show-posts.blade.php
@@ -16,7 +16,7 @@ new class extends Component {
 <div class="mb-10">
     <x-home-section :title="__('Featured Posts')">
         @foreach ($featuredPosts as $post)
-            <x-post-card :post="$post" />
+            <x-post-card wire:key='{{ $post->id }}' :post="$post" />
         @endforeach
     </x-home-section>
 
@@ -24,7 +24,7 @@ new class extends Component {
 
     <x-home-section :title="__('Latest Posts')">
         @foreach ($latestPosts as $post)
-            <x-post-card :post="$post" />
+            <x-post-card wire:key='{{ $post->id }}' :post="$post" />
         @endforeach
     </x-home-section>
 </div>
